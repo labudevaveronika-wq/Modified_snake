3fec31da6852823dde2771aa55011d52e6170690
labudevaveronika-wq <labudevaveronika@gmail.com>
2025-11-16 13:00:27 +0300

    чиста и тест

--- sanke.py
+++ sanke.py
@@ -14,7 +14,12 @@ class Snake:
         Проверка выхода за границы.'''
 
         if self.head in self.position[1::]:
-            return False '''змея мертва (врезалась в себя)'''
+            return False # змея мертва (врезалась в себя)'
+
+        if (self.head[0] < 0 or self.head[0] >= self.size_table) or (self.head[1] < 0 or self.head[1] >= self.size_table):
+            return False # змея мертва (вне поля)
+
+        return True # Все хорошо змея жива, пока что...'
 
 
 
@@ -23,29 +28,31 @@ class Snake:
         Перемещать змейку на одну клетку в текущем направлении.
         Брать позицию головы и добавлять направление движения.
         Обновлять переменную self.head.'''
-        nx = self.head[0] + self.direction[0]
-        '''Обновление позиции головы по х'''
-        ny = self.head[1] + self.direction[1]
-        '''Обновление позиции головы по у'''
+        nx = self.head[0] + self.direction[0] # Обновление позиции головы по х
+        ny = self.head[1] + self.direction[1] # Обновление позиции головы по у
 
-        self.position.insert(0, (nx, ny))
-        '''Добавление "новой головы" в змею'''
+        self.position.insert(0, (nx, ny)) # Добавление "новой головы" в змею
 
-        self.head = self.position[0]
-        '''Обновление головы, а точнее ее позиции'''
+        self.head = self.position[0] # Обновление головы, а точнее ее позиции
 
-        '''Удаление хвоста, если длина позиций больше self.snake_body'''
+        if len(self.position) > self.snake_body:
+            self.position.pop() # Удаление хвоста, если длина позиций больше self.snake_body
 
 
 
     def eat(self, food_type):
         '''Процесс, который будет обрабатывать то, что съела змея.
         Обновление счетчика очков для комбо-еды.'''
-        if food_type == 'apple': self.snake_body = self.snake_body + 1 ''''''
-        if food_type == 'pear': self.snake_body = self.snake_body - 1 ''''''
-        if food_type == 'grape': '''Активация ускорение для змеи'''
+        if food_type == 'apple':
+            self.snake_body += 1 # Красное яблоко - длина + 1
+        if food_type == 'pear':
+            self.snake_body -= 1 # Зеленая груша - длина - 1
+        if food_type == 'grape':
+            pass
+            '''Активация ускорение для змеи'''
 
-        '''Возвращение эффекта для вижуала.'''
+        return  food_type
+        '''Возвращение эффекта для вижуала змеи.'''
 
 
     def drow(self, ):
@@ -57,6 +64,7 @@ class Snake:
         '''Квадратики для змеи на поле в ячейках по значению self.position'''
         '''Голова чуть другого оттенка, в сравнении с телом'''
         '''Еда и границы'''
+        pass
 
 
     def moving(self, direction_type):
@@ -64,5 +72,14 @@ class Snake:
         Проверка разворота на 180.
         Обновление self.direction'''
 
-        self.direction = direction_type '''Обновление направления'''
+        if (direction_type[0] * -1, direction_type[1] * -1) != self.direction: # Проверка разворота на 180°
+            self.direction = direction_type # Обновление направления
 
+
+# Простой тест
+#snake = Snake()
+#print(snake.state())
+#snake.simple_move()
+#print(snake.head)
+#True
+#(11, 10)
