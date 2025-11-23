846973669d9a570204456405731ebd6a4dd4e375
labudevaveronika-wq <labudevaveronika@gmail.com>
2025-11-23 13:42:16 +0300

    исправила Obstacle

--- obstacle.py
+++ obstacle.py
@@ -6,18 +6,18 @@ class Obstacle:
         for i in range(count):
             self.obstacles.append(self.create_obstacke())
 
-        def create_obstacke(self):
-            x = randint(0, 14)
-            y = randint(0, 14)
-            width = 10
-            hight = 20
+    def create_obstacke(self):
+        x = randint(0, 14)
+        y = randint(0, 14)
+        width = 10
+        hight = 20
 
-            return {
-                'position': (x, y),
-                'width': width,
-                'height': hight,
-                'color': randint(100, 100, 100),
-            }
+        return {
+            'position': (x, y),
+            'width': width,
+            'height': hight,
+            'color': randint(100, 100, 100),
+        }
 
-        def get_all_obstacles(self):
-            return self.obstacles
+    def get_all_obstacles(self):
+        return self.obstacles
