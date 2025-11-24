4176a19a69839856687a05ed2e7baf6045f6a940
labudevaveronika-wq <labudevaveronika@gmail.com>
2025-11-25 00:19:03 +0300

    добавила обработку на то чтобы препятсвие не появлялось в момент начала игры в голове у змеи

--- obstacle.py
+++ obstacle.py
@@ -1,14 +1,22 @@
 from random import *
+from sanke import Snake
 
 class Obstacle:
-    def __init__(self, count = 7):
+    def __init__(self, count = 7, snake_positions=None):
         self.obstacles = []
         for i in range(count):
-            self.obstacles.append(self.create_obstacle())
+            self.obstacles.append(self.create_obstacle(snake_positions))
 
-    def create_obstacle(self):
+    def create_obstacle(self, snake_positions=None):
+
         x = randint(0, 14)
         y = randint(0, 14)
+
+        if snake_positions:
+            while (x, y) in snake_positions:
+                y = randint(0, 14)
+                x = randint(0, 14)
+
         width = 40
         hight = 40
 
